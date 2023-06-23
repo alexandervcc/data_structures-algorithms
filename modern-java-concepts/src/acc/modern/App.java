abd3bcb074293b527615f9c563bdf7f061bdf454
Alex Charco Cuenca <alex.charco.25@gmail.com>
2023-06-23 16:42:26 -0500

    Modern Java Concepts

---
+++ modern-java-concepts/src/acc/modern/App.java
@@ -0,0 +1,9 @@
+package acc.modern;
+
+import acc.modern.lambdas.letter.LetterTest;
+
+public class App {
+  public static void main(String[] args) throws Exception {
+      LetterTest.runExample();
+    }
+}
